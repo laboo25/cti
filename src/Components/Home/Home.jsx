667ffda27e7b88f47e08e1370f18ca48d26f0edd
lab00 <89362256+laboo25@users.noreply.github.com>
2023-12-05 04:32:03 +0600

    Add files via upload

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -70,32 +70,36 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="containerwr bg-[#dedede] mx-auto">
-                {filteredAndSearchedData.map((item) => (
-    <div className="cards" key={item.id}>
-        <div className="title w-full p-[5px]" style={{ backgroundColor: item.ncol }}>
-            <h2 className="text-[18px] font-bold capitalize">
-                {item.name}
-            </h2>
-        </div>
-        <div className="w-full h-[100px] p-[5px] overflow-hidden bg-[#677077] border-b-[1px] border-black">
-            <Link to={item.to}>
-                <p className="text-[12px] text-[#3f464c] text-justify">{item.description}</p>
-            </Link>
-        </div>
-        <div className="tag w-full h-auto p-[5px] bg-[#677077]">
-            <ul className="text-[13px] uppercase text-[blue] flex">
-                {item.catagory.map((categoryItem) => (
-                    <li className='mr-2' key={categoryItem}>{categoryItem}</li>
-                ))}
-            </ul>
-        </div>
-    </div>
-))}
+                    {filteredAndSearchedData.map((item) => (
+                        <div className="cards" key={item.id}>
+                            <div className="title w-full p-[5px]" style={{ backgroundColor: item.ncol }}>
+                                <h2 className="text-[18px] font-bold capitalize">
+                                    {item.name}
+                                </h2>
+                            </div>
+                            <div className="w-full h-[100px] p-[5px] overflow-hidden bg-[#677077] border-b-[1px] border-black">
+                                <Link to={item.to}>
+                                    <p className="text-[12px] text-[#3f464c] text-justify">{item.description}</p>
+                                </Link>
+                            </div>
+                            <div className="tag w-full h-auto p-[5px] bg-[#677077]">
+                                <ul className="text-[13px] uppercase text-[blue] flex">
+                                    {item.catagory.map((categoryItem) => (
+                                        <li className='mr-2' key={categoryItem}>{categoryItem}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    ))}
 
                 </div>
-                <div className="pagination">
+                <div className="pagination flex justify-center py-3">
                     {Array.from({ length: Math.ceil(filteredAndSearchedData.length / itemsPerPage) }, (_, index) => (
-                        <button key={index + 1} onClick={() => paginate(index + 1)}>
+                        <button
+                            key={index + 1}
+                            onClick={() => paginate(index + 1)}
+                            className={currentPage === index + 1 ? 'active bg-red-600' : ''}
+                        >
                             {index + 1}
                         </button>
                     ))}
