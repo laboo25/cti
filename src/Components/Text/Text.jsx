27c0a0b94286f74218c5a4799608a431be9b1a36
lab00 <89362256+laboo25@users.noreply.github.com>
2023-12-05 03:00:36 +0600

    Add files via upload

--- src/Components/Text/Text.jsx
+++ src/Components/Text/Text.jsx
@@ -9,8 +9,8 @@ const Text = () => {
 
   return (
     <div>
-      <div className='w-full flex justify-center'>
-        <div className='w-[600px] bg-blue-400 p-5'>
+      <div className='w-full flex justify-center bg-[#6d6d6d]'>
+        <div className='w-[600px] bg-[#0f172a] p-5'>
           {/* <button onClick={() => history.goBack()}>
             Go Back{username}
           </button> */}
@@ -19,7 +19,20 @@ const Text = () => {
                 Data.map((item)=>(
                   <div className='w-full' key={item.id}>
               <h1 className='text-[30px] my-4'>{item.name}</h1>
-              <p className='text-[15px] text-justify font-noto font-semibold'>
+              <p className='text-[12px] text-justify font-noto font-light uppercase  py-[10px]'>
+              {
+                item.catagory.map((catagory)=>{
+                  return (
+                    
+                      <span className='mr-3' key={catagory}>
+                      {catagory}
+                    </span>
+                    
+                  )
+                })
+              }
+              </p>
+              <p className='text-[15px] text-justify font-noto font-semibold text-[#6d6d6d] leading-8'>
                 {item.text}
               </p>
             </div>
