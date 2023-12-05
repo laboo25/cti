b9b7e42c14071cfd846b023e194ef5eb02e6b744
lab00 <89362256+laboo25@users.noreply.github.com>
2023-12-05 21:05:28 +0600

    Add files via upload

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -52,7 +52,7 @@ const Home = () => {
         <>
             <div>
                 <div>
-                    <div className="filter w-full flex flex-wrap flex-col">
+                    <div className="filter">
                         <input
                             type="text"
                             placeholder="search"
