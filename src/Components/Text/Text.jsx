f7751efa648dd77bb7655384a3a2c7beca8783c3
lab00 <89362256+laboo25@users.noreply.github.com>
2023-12-16 20:22:38 +0600

    Update Text.jsx

--- src/Components/Text/Text.jsx
+++ src/Components/Text/Text.jsx
@@ -1,33 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import "../Text/text.css"
+import "../Text/text.css";
 import { Link, useParams } from 'react-router-dom';
 import { CtiTxt } from '../Text/CtiTxt';
 
 const Text = () => {
   const { title } = useParams();
   const [showBackToTop, setShowBackToTop] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   const item = CtiTxt.find((item) => item.title === title);
 
-  if (!item) {
-    return <div className="flex items-center justify-center h-screen">
-    <div className="text-center">
-      <h1 className="text-6xl font-bold mb-4 text-red-500">404</h1>
-      <p className="text-xl font-semibold mb-4">Page not found</p>
-      <p className="text-gray-600 mb-8">The page you are looking for might be in another castle.</p>
-      <Link href="/">
-        <a className="text-blue-500 hover:underline">Go back home</a>
-      </Link>
-    </div>
-  </div>;
-  }
-
-  const handleScroll = () => {
-    const scrollY = window.scrollY;
-    setShowBackToTop(scrollY > window.innerHeight);
-  };
-
   useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      setShowBackToTop(scrollY > window.innerHeight);
+    };
+
     window.addEventListener('scroll', handleScroll);
 
     return () => {
@@ -35,33 +23,47 @@ const Text = () => {
     };
   }, []);
 
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
   return (
     <>
-      <div className='w-full'>
+      <div className={`w-full ${isDarkMode ? 'dark-theme' : 'light-theme'}`}>
         <div className=' textcontainer bg-gray-500'>
-        <div className='coverph w-full h-[250px]'>
-          <img src={item.cover} alt="" className='w-full h-full object-contain' />
+          <div className='coverph w-full h-[250px]'>
+            <img src={item.cover} alt="" className='w-full h-full object-contain' />
+          </div>
+          <div className={`w-full p-2 ${isDarkMode ? 'bg-gray-900' : 'bg-red-500'}`}>
+            <h1 className={`text-[30px] font-bold my-1 mb-6 ${isDarkMode ? 'text-white' : 'text-red-500'}`} style={{ color: item.ncol }}>
+              {item.name}
+            </h1>
+            <div className={`text font-noto leading-8 w-full ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-blue-400'}`}>
+              {item.text}
+            </div>
+          </div>
         </div>
-        <div className='w-full bg-red-500 p-2'>
-          <h1 className='text-[30px] font-bold my-1 mb-6' style={{ color: item.ncol }}>{item.name}</h1>
-          <div className='text font-noto leading-8 w-full bg-blue-400'>{item.text}</div>
-        </div>
-      </div>
 
-      {showBackToTop && (
-        <button className='w-6 h-6 fixed bottom-8 right-8 bg-[#ffffff48] text-black rounded-full'
-                onClick={scrollToTop}>
+        <button
+          className={`w-6 h-6 fixed bottom-8 right-8 rounded-full ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-[#ffffff48] text-black'}`}
+          onClick={scrollToTop}
+        >
           ▲
         </button>
-      )}
+
+        <button
+          className={`absolute top-4 right-4 p-2 rounded-full ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-[#ffffff48] text-black'}`}
+          onClick={toggleTheme}
+        >
+          {isDarkMode ? '🌞' : '🌙'}
+        </button>
       </div>
     </>
   );
 };
 
 export default Text;
-
